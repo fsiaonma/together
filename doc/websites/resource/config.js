98a16c92b8ac7527f831f7b0d71d516fb1264704
junhao <28962383@qq.com>
2013-03-21 18:05:51 +0800

    提交代码 websites
    提交代码 websites

---
+++ doc/websites/resource/config.js
@@ -0,0 +1,7 @@
+var list = [{
+    name: 'md 文件格式',
+    href: 'http://www.cnblogs.com/catcherx/archive/2013/02/14/2911166.html'
+},{
+    name: 'md to html',
+    href: 'http://www.ituring.com.cn/article/10044'
+}];
