52ad8c3d1eb5758b069fb24cd628c5e87fb8a59e
fsiaonma <root@sam.(none)>
2013-03-30 12:38:14 +0800

    commit demo

--- demo/eagleMysql/eagleMysqlDemo.cpp
+++ demo/eagleMysql/eagleMysqlDemo.cpp
@@ -6,50 +6,50 @@ using namespace std;
 
 int main() {
 	// init
-	eagleMysql e("localhost", "root", "123456", "testmysql", 3306);
+    eagleMysql e("localhost", "root", "123456", "testmysql", 3306);
 
 	// insert
-	PARAM dataA;
-	dataA.key = "username";
-	dataA.value = "sam";
-	PARAM dataB;
-	dataB.key = "password";
-	dataB.value = "123456";
+    PARAM dataA;
+    dataA.key = "username";
+    dataA.value = "sam";
+    PARAM dataB;
+    dataB.key = "password";
+    dataB.value = "123456";
 
-	PARAMLIST insertList;
-	insertList.push_back(dataA);
-	insertList.push_back(dataB);
+    PARAMLIST insertList;
+    insertList.push_back(dataA);
+    insertList.push_back(dataB);
 
-	e.insert("user", insertList);
+    e.insert("user", insertList);
 
 	// remove
-	e.remove("user", "where username = 'sam'");
+    e.remove("user", "where username = 'sam'");
 
 	// update
-	PARAM dataC;
-	dataC.key = "username";
-	dataC.value = "testUpdate";
-	PARAMLIST testUpdateInsertList;
-	testUpdateInsertList.push_front(dataC);
-	e.insert("user", testUpdateInsertList);
+    PARAM dataC;
+    dataC.key = "username";
+    dataC.value = "testUpdate";
+    PARAMLIST testUpdateInsertList;
+    testUpdateInsertList.push_front(dataC);
+    e.insert("user", testUpdateInsertList);
 
-	PARAM dataD;
-	dataD.key = "username";
-	dataD.value = "fsiaonma";
-	PARAM dataE;
-	dataE.key = "password";
-	dataE.value = "123456789";
-	PARAMLIST updateList;
-	updateList.push_back(dataD);
-	updateList.push_back(dataE);
-	e.update("user", updateList, "where username = 'testUpdate'");
+    PARAM dataD;
+    dataD.key = "username";
+    dataD.value = "fsiaonma";
+    PARAM dataE;
+    dataE.key = "password";
+    dataE.value = "123456789";
+    PARAMLIST updateList;
+    updateList.push_back(dataD);
+    updateList.push_back(dataE);
+    e.update("user", updateList, "where username = 'testUpdate'");
 
 	//excute
-	e.connet();
+    e.connet();
 
-	MYSQL mysql;
-	string sql = "select * from user;";
-	mysql = e.excute(sql);
+    MYSQL mysql;
+    string sql = "select * from user;";
+    mysql = e.excute(sql);
 
 	MYSQL_RES *result = NULL;
 	MYSQL_FIELD *field = NULL;
