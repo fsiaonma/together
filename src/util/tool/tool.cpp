ae9baa06e14abfe61ba97e78abb54e27f82cd023
root <root@kc.(none)>
2013-04-07 22:42:28 +0800

    md5_str

--- src/util/tool/tool.cpp
+++ src/util/tool/tool.cpp
@@ -27,3 +27,19 @@ int Tool::S2I(string num, int default_val)
 		return default_val;
 	}
 }
+
+int Tool::md5_str(char *s,char *md5_sum)
+{
+    char c[50];
+    sprintf(c, "printf '%s' | md5sum | cut -d ' ' -f1", s);
+    FILE *p = popen(c, "r");
+    if (p == NULL) 
+        return 0;
+    int i, ch;
+    for (i = 0; i < 32 && isxdigit(ch = fgetc(p)); i++) {
+        *md5_sum++ = ch;
+    }
+    *md5_sum = '\0';
+    pclose(p);
+    return i == 32;
+}
--- src/util/tool/tool.h
+++ src/util/tool/tool.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <stdlib.h>
 #include <sstream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -16,6 +17,8 @@ public:
 	static int getArrayLen(T &array);
 
 	static int S2I(string num, int default_val = -1);
+
+	static int md5_str(char *s,char *md5_sum);
 };
 
 #endif
