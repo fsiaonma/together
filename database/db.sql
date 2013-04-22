c810d58de3e6233eaa0df576b73ce6be695cb541
root <root@kc.(none)>
2013-04-22 23:40:07 +0800

    update database ....

--- database/db.sql
+++ database/db.sql
@@ -2,16 +2,16 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `testmysql` ;
-CREATE SCHEMA IF NOT EXISTS `testmysql` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `testmysql` ;
+DROP SCHEMA IF EXISTS `together` ;
+CREATE SCHEMA IF NOT EXISTS `together` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `together` ;
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_file`
+-- Table `together`.`t_file`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_file` ;
+DROP TABLE IF EXISTS `together`.`t_file` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_file` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_file` (
   `id` INT NOT NULL ,
   `suffix` VARCHAR(45) NULL ,
   `md5` VARCHAR(45) NULL ,
@@ -22,11 +22,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_user`
+-- Table `together`.`t_user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_user` ;
+DROP TABLE IF EXISTS `together`.`t_user` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_user` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_user` (
   `id` INT NOT NULL ,
   `username` VARCHAR(30) NULL ,
   `password` VARCHAR(20) NULL ,
@@ -40,18 +40,18 @@ CREATE  TABLE IF NOT EXISTS `testmysql`.`t_user` (
   INDEX `fk_t_user_t_file1_idx` (`signature_record_id` ASC) ,
   CONSTRAINT `fk_t_user_t_file1`
     FOREIGN KEY (`signature_record_id` )
-    REFERENCES `testmysql`.`t_file` (`id` )
+    REFERENCES `together`.`t_file` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_address`
+-- Table `together`.`t_address`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_address` ;
+DROP TABLE IF EXISTS `together`.`t_address` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_address` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_address` (
   `id` INT NOT NULL ,
   `addr_type` INT NULL ,
   `longitude` FLOAT NULL COMMENT '经度' ,
@@ -62,11 +62,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_room`
+-- Table `together`.`t_room`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_room` ;
+DROP TABLE IF EXISTS `together`.`t_room` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_room` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_room` (
   `id` INT NOT NULL ,
   `tittle` VARCHAR(255) NULL ,
   `type` INT NULL ,
@@ -87,33 +87,33 @@ CREATE  TABLE IF NOT EXISTS `testmysql`.`t_room` (
   INDEX `fk_t_room_t_user1_idx` (`owner_id` ASC) ,
   CONSTRAINT `fk_t_room_t_address`
     FOREIGN KEY (`addr_id` )
-    REFERENCES `testmysql`.`t_address` (`id` )
+    REFERENCES `together`.`t_address` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_room_t_file1`
     FOREIGN KEY (`record_id` )
-    REFERENCES `testmysql`.`t_file` (`id` )
+    REFERENCES `together`.`t_file` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_room_t_file2`
     FOREIGN KEY (`preview_pic_id` )
-    REFERENCES `testmysql`.`t_file` (`id` )
+    REFERENCES `together`.`t_file` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_room_t_user1`
     FOREIGN KEY (`owner_id` )
-    REFERENCES `testmysql`.`t_user` (`id` )
+    REFERENCES `together`.`t_user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_room_user_relation`
+-- Table `together`.`t_room_user_relation`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_room_user_relation` ;
+DROP TABLE IF EXISTS `together`.`t_room_user_relation` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_room_user_relation` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_room_user_relation` (
   `id` INT NOT NULL ,
   `room_id` INT NULL ,
   `user_id` INT NULL COMMENT '房间用户关系表' ,
@@ -122,23 +122,23 @@ CREATE  TABLE IF NOT EXISTS `testmysql`.`t_room_user_relation` (
   INDEX `fk_t_room_user_relation_t_user1_idx` (`user_id` ASC) ,
   CONSTRAINT `fk_t_room_user_relation_t_room1`
     FOREIGN KEY (`room_id` )
-    REFERENCES `testmysql`.`t_room` (`id` )
+    REFERENCES `together`.`t_room` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_room_user_relation_t_user1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `testmysql`.`t_user` (`id` )
+    REFERENCES `together`.`t_user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `testmysql`.`t_follow`
+-- Table `together`.`t_follow`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `testmysql`.`t_follow` ;
+DROP TABLE IF EXISTS `together`.`t_follow` ;
 
-CREATE  TABLE IF NOT EXISTS `testmysql`.`t_follow` (
+CREATE  TABLE IF NOT EXISTS `together`.`t_follow` (
   `id` INT NOT NULL ,
   `followed_id` INT NULL COMMENT '被关注人id' ,
   `follow_id` INT NULL COMMENT '关注人id' ,
@@ -147,17 +147,17 @@ CREATE  TABLE IF NOT EXISTS `testmysql`.`t_follow` (
   INDEX `fk_t_follow_t_user2_idx` (`follow_id` ASC) ,
   CONSTRAINT `fk_t_follow_t_user1`
     FOREIGN KEY (`followed_id` )
-    REFERENCES `testmysql`.`t_user` (`id` )
+    REFERENCES `together`.`t_user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_t_follow_t_user2`
     FOREIGN KEY (`follow_id` )
-    REFERENCES `testmysql`.`t_user` (`id` )
+    REFERENCES `together`.`t_user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `testmysql` ;
+USE `together` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
